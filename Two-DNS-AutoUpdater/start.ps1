ae5264bb9d638c1b90ef22752dbac76cbd052a4c
Simon F <github@19r.de>
2017-06-06 16:37:36 +0200

    v1.0
    1. Version des lauffähigen Scripts fertig gestellt.

--- Two-DNS-AutoUpdater/start.ps1
+++ Two-DNS-AutoUpdater/start.ps1
@@ -23,9 +23,10 @@ cls
 ##################################################  ##################################################
 ##################################################  ##################################################
 
-$ver = "v0.4"
+$ver = "v1.0"
 
 $host.ui.RawUI.WindowTitle = “Two-DNS Auto Updater $ver  –  © github.simonfieber.it”
+$ErrorActionPreference = "SilentlyContinue"
 
 ### Startbildschirm ###
 function startbildschirm {
@@ -44,3 +45,36 @@ function Send-UpdateRequest{
         Invoke-WebRequest -Uri $updateHTTP -Headers @{"Authorization" = "Basic "+[System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($username+":"+$password ))} -Method Get
     }
 }
+
+$UR = Send-UpdateRequest
+
+function Set-DynamicIP{
+    startbildschirm
+    Start-Sleep -Milliseconds 1500
+    if($UR.StatusCode -eq "200"){
+        Write-Host "    ╔═══════════════════════════════════════════════════════════════════════════════╗"
+        Write-Host "    ║ Anfrage wurde erfolgreich gesendet!                                           ║"
+        Write-Host "    ║                                                                               ║"
+        Write-Host "    ╚═══════════════════════════════════════════════════════════════════════════════╝"
+        Write-Host ""
+        Write-Host " Rückmeldung des Servers:"
+        Write-Host ""
+        Write-Host " $UR"
+        Write-Host ""
+        Write-Host ""
+        Start-Sleep -Milliseconds 10000
+    }
+    else {
+        Write-Host "    ╔═══════════════════════════════════════════════════════════════════════════════╗"
+        Write-Host "    ║ Es ist ein unbekannter Fehler aufgetreten!                                    ║"
+        Write-Host "    ║                                                                               ║"
+        Write-Host "    ╚═══════════════════════════════════════════════════════════════════════════════╝"
+        $Request
+        Write-Host ""
+        Write-Host ""
+        Start-Sleep -Milliseconds 5000
+    }
+}
+
+Set-DynamicIP
+[Environment]::Exit(1)
